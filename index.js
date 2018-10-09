5d754262d91fa5f7a4ae18e5f45f1b13d719049e
Emily Friedlander <emily@emilyfriedlander.com>
2018-10-09 00:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+   fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json) + console.log(json))
+    
 }
 
 function renderBooks(json) {
+  let counter = 0;
   const main = document.querySelector('main')
   json.forEach(book => {
+
+// start new expression    
+    if(counter === 4){
+    const fifthBook = document.createElement('h1');
+    fifthBook.innerHTML = `<h2> This ${book.name} is the 5th book</h2>`
+    main.appendChild(fifthBook)
+    }
+// end of new expresssion     
+
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
+    counter++
   })
+  
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
